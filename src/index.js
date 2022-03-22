c35a8eeecbdf0ce7bee54a7ff95f07f091a1f628
Ashis-Thorium <ashisbisoi05@gmail.com>
2022-03-23 00:07:44 +0530

    Ashis

--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const mongoose = require('mongoose')
 
-mongoose.connect("", {useNewUrlParser: true})
+mongoose.connect("mongodb+srv://As_357:oJAKh3z4S39UgaUZ@cluster0.k4tlh.mongodb.net/group57database?authSource=admin&replicaSet=atlas-vwbpyb-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true", {useNewUrlParser: true})
     .then(() => console.log('mongodb running on 27017'))
     .catch(err => console.log(err))
 
-app.use('/functionup', route);
+app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function() {
 	console.log('Express app running on port ' + (process.env.PORT || 3000))
---
+++ src/models/collegeModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const collegeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'name is required'],
+        unique: true,
+        trim: true
+    },
+    fullName: {
+        type: String,
+        required: [true, 'fullname is required'],
+        trim: true
+    },
+    logoLink: {
+        type: String,
+        required: [true, 'logolink is required'],
+        trim: true
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+},
+    { timestamps: true })
+
+module.exports = mongoose.model('CollegeDb', collegeSchema)
